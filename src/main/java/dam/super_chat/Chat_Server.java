8e3797bbc7a2bb6afdce79c2cea0fb812cc26706
Manuel Zambudio <manuel.zambudio.herrerias@gmail.com>
2025-03-21 13:48:08 +0100

    Día 1 (Clase)
    Entendiendo los códigos del profesor

--- src/main/java/dam/super_chat/Chat_Server.java
+++ src/main/java/dam/super_chat/Chat_Server.java
@@ -3,11 +3,15 @@
  */
 package dam.super_chat;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.time.LocalDateTime;
 
 /**
  *
@@ -15,25 +19,36 @@ import java.net.Socket;
  */
 public class Chat_Server {
 
+    // Objetos para la conexion
     private ServerSocket serverSocket;
     private Socket socket;
     private InputStream is;
     private OutputStream os;
 
+    // Objetos para envío y recepción de cadenas
+    private InputStreamReader isr;
+    private BufferedReader br;
+    private PrintWriter pw;
+
+    // Constructor
     public Chat_Server(int puerto) throws IOException {
         serverSocket = new ServerSocket(puerto);
     }
 
+    // Espera conexiones
     public void start() throws IOException {
         System.out.println(" (Servidor) Esperando conexiones...");
         socket = serverSocket.accept(); // Queda a la espera de una conexion
         is = socket.getInputStream();   // Abre flujos de lectura
         os = socket.getOutputStream();  // Abre flujos de escritura
+        abrirCanalesDeTexto();          // Abre los canales de texto
         System.out.println(" (Servidor) Conexión establecida con cliente " + socket.getRemoteSocketAddress());
     }
 
+    // Cierra todo
     public void stop() throws IOException {
         System.out.println(" (Servidor) Cerrando conexiones...");
+        cerrarCanalesDeTexto(); // Cierra los canales de texto
         is.close();             // Cierra flujos de lectura
         os.close();             // Cierra flujos de escritura
         socket.close();         // Cierra el Socket
@@ -41,17 +56,64 @@ public class Chat_Server {
         System.out.println(" (Servidor) Conexiones cerradas.");
     }
 
-    public static void main(String[] args) {
+    // Abre los canales de texto
+    public void abrirCanalesDeTexto() {
+        System.out.println(" (Servidor) Abriendo canales de texto...");
+        // Lectura
+        isr = new InputStreamReader(is);
+        br = new BufferedReader(isr);
+        // Escritura
+        pw = new PrintWriter(os, true);
+        System.out.println(" (Servidor) Canales de texto abiertos.");
+    }
 
+    // Cierra los canales de texto
+    public void cerrarCanalesDeTexto() throws IOException {
+        System.out.println(" (Servidor) Cerrando canales de texto...");
+        // Lectura
+        br.close();
+        isr.close();
+        // Escritura
+        pw.close();
+        System.out.println(" (Servidor) Canales de texto cerrados.");
+    }
+
+    // Devuelve el mensaje enviado por el cliente como String
+    public String leerMensajeDeTexto() throws IOException {
+        System.out.println(" (Servidor) Leyendo mensaje...");
+        String msg = br.readLine();
+        System.out.println(" (Servidor) Mensaje leído.");
+        return msg;
+    }
+
+    // Envía un mensaje al cliente mediante PrintWriter(OutPutStream)
+    public void enviarMensajeDeTexto(String msg) {
+        System.out.println(" (Servidor) Eviando mensaje...");
+        pw.println(msg);
+        System.out.println(" (Servidor) Mensaje enviado.");
+    }
+
+    public static void main(String[] args) {
         try {
+            // Inicio
             Chat_Server server = new Chat_Server(50000);
             server.start();
-            System.out.println("Mensaje del cliente: " + server.is.read());
-            server.os.write(200);
+
+            // Recepción del mensaje del cliente
+            String msg = server.leerMensajeDeTexto();
+            System.out.println(" (Servidor) Mensaje del cliente: " + msg);
+
+            // Envío de confirmación al cliente
+            LocalDateTime horaLocal = LocalDateTime.now();
+            int horas = horaLocal.getHour();
+            int minutos = horaLocal.getMinute();
+            int segundos = horaLocal.getSecond();
+            server.enviarMensajeDeTexto("ACK " + horas + ":" + minutos + ":" + segundos);
+
+            // Cierre de todo
             server.stop();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 }
